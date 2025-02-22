081337d046791fa550f14d521dc44466e70b72cc
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-02-22 22:16:41 +0700

    Membuat component announcement

---
+++ src/components/announcement/Announcement.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Announcement.css";
+
+const Announcement = () => {
+  return (
+    <div className="announcement-container">
+      <h1>Pengiriman Gratis</h1>
+      <h1>Penawaran selamat datang diskon 15%</h1>
+      <h1>Pengembalian Gratis</h1>
+    </div>
+  );
+};
+
+export default Announcement;
