ae982ee9756b842f6ec878d828b6a42a25d7ed5f
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-02-24 15:21:37 +0700

    Menambah route path products

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./pages/home/Home";
 import Register from "./pages/register/Register";
 import Login from "./pages/login/Login";
+import AllProducts from "./pages/all product/AllProducts";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/products" element={<AllProducts />} />
       </Routes>
     </Router>
   );
