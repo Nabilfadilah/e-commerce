e5e80ca1f94f53adb7345acb496b25e4fc197ddc
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-02-24 09:48:24 +0700

    Import component navbar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Announcement from "../../components/announcement/Announcement";
+import Navbar from "../../components/navbar/Navbar";
+import Slider from "../../components/slider/Slider";
 
 function Home() {
   return (
     <div>
       <Announcement />
+      <Navbar />
     </div>
   );
 }
