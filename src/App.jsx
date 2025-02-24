5634e387897827d4bb36e9611d916b7917e63ff1
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-02-24 14:35:08 +0700

    Merubah router path menggunakan react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home/Home";
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
