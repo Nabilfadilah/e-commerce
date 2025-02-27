13a58f5d475624ed831cd16e017bbb183f4d2804
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-02-27 11:00:48 +0700

    Membuat data json product

---
+++ src/data.js
@@ -0,0 +1,227 @@
+import slider1 from "./assets/slider1.jpg";
+import slider2 from "./assets/slider2.jpg";
+import slider3 from "./assets/slider3.jpg";
+
+import category1 from "./assets/category1.jpg";
+import category2 from "./assets/category2.jpg";
+import category3 from "./assets/category3.jpg";
+
+import products1 from "./assets/products1.jpeg";
+import products2 from "./assets/products2.jpeg";
+import products3 from "./assets/products3.jpeg";
+import products4 from "./assets/products4.jpeg";
+import products5 from "./assets/products5.jpeg";
+import products6 from "./assets/products6.jpeg";
+import products7 from "./assets/products7.jpeg";
+import products8 from "./assets/products8.jpeg";
+import products9 from "./assets/products9.jpeg";
+import products10 from "./assets/products10.jpeg";
+import products11 from "./assets/products11.jpeg";
+import products12 from "./assets/products12.jpeg";
+import products13 from "./assets/products13.jpeg";
+import products14 from "./assets/products14.jpeg";
+import products15 from "./assets/products15.jpeg";
+
+export const sliderItems = [
+  {
+    id: 1,
+    img: slider1,
+    title: "Koleksi Sepatu",
+    desc: "Tren Saat Ini: Motif yang Mencolok. Diskon hingga 50% untuk Item Pilihan",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: slider2,
+    title: "Sedang Tren",
+    desc: "Produk Terlaris: Barang yang Wajib Dimiliki. Jangan Sampai Ketinggalan!",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: slider3,
+    title: "Beragam Koleksi",
+    desc: "Tingkatkan Gaya Anda dengan Koleksi Terbaru Kami. Diskon hingga 50% untuk Item Pilihan",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: category1,
+    title: "Produk Terbatas",
+  },
+  {
+    id: 2,
+    img: category2,
+    title: "Produk Terbaru",
+  },
+  {
+    id: 3,
+    img: category3,
+    title: "Product Terlaris",
+  },
+];
+
+export const newArrivals = [
+  {
+    id: 3,
+    img: products3,
+    title: "Vans Knu Skool",
+    price: 759.000,
+  },
+  {
+    id: 4,
+    img: products4,
+    title: "Vans Era",
+    price: 779.000,
+  },
+  {
+    id: 6,
+    img: products6,
+    title: "Vans Sk8-Hi ",
+    price: 999.000,
+  },
+];
+
+export const bestsellers = [
+  {
+    id: 7,
+    img: products7,
+    title: "Vans Checkerboard Slip On",
+    price: 629.000,
+  },
+  {
+    id: 1,
+    img: products1,
+    title: "Vans Old Skool",
+    price: 780.000,
+  },
+  {
+    id: 12,
+    img: products12,
+    title: "Vans Slip On",
+    price: 670.000,
+  },
+];
+
+export const allProducts = [
+  {
+    id: 1,
+    img: products1,
+    title: "Vans Old Skool",
+    price: 780.000,
+  },
+  {
+    id: 2,
+    img: products2,
+    title: "Vans Sk8-Hi Black and White",
+    price: 990.000,
+  },
+  {
+    id: 3,
+    img: products3,
+    title: "Vans Knu Skool",
+    price: 759.000,
+  },
+  {
+    id: 4,
+    img: products4,
+    title: "Vans Era",
+    price: 779.000,
+  },
+  {
+    id: 5,
+    img: products5,
+    title: "Vans Slip On",
+    price: 640.000,
+  },
+  {
+    id: 6,
+    img: products6,
+    title: "Vans Sk8-Hi ",
+    price: 999.000,
+  },
+  {
+    id: 7,
+    img: products7,
+    title: "Vans Checkerboard Slip On",
+    price: 629.000,
+  },
+  {
+    id: 8,
+    img: products8,
+    title: "Vans Old Skool",
+    price: 780.000,
+  },
+  {
+    id: 9,
+    img: products9,
+    title: "Vans Authentic ",
+    price: 650.000,
+  },
+  {
+    id: 10,
+    img: products10,
+    title: "Vans Old Skool",
+    price: 790.000,
+  },
+  {
+    id: 11,
+    img: products11,
+    title: "Vans Slip On",
+    price: 670.000,
+  },
+  {
+    id: 12,
+    img: products12,
+    title: "Vans Slip On",
+    price: 670.000,
+  },
+  {
+    id: 13,
+    img: products13,
+    title: "Vans Checkerboard Slip On",
+    price: 670.000,
+  },
+  {
+    id: 14,
+    img: products14,
+    title: "Vans Slip On",
+    price: 670.000,
+  },
+  {
+    id: 15,
+    img: products15,
+    title: "Vans Slip On",
+    price: 670.000,
+  },
+];
+
+export const footerLinks = [
+  {
+    id: 1,
+    title: "Home",
+  },
+  {
+    id: 2,
+    title: "Cart",
+  },
+  {
+    id: 3,
+    title: "Accessories",
+  },
+  {
+    id: 4,
+    title: "My Account",
+  },
+  {
+    id: 5,
+    title: "Order Tracking",
+  },
+  {
+    id: 6,
+    title: "Wishlist",
+  },
+];
